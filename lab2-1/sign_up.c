2b84e33c177ecfd1e767c97c23778bc46d39f7aa
norvakent <hjlee100679@gmail.com>
2025-09-10 15:34:55 +0900

    lab 2-1

---
+++ lab2-1/sign_up.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+  char id[20];
+  char pw[20];
+  char check[20];
+
+  printf("Write ID\n");
+  scanf("%s", id);
+  printf("your ID is %s\n", id);
+
+  printf("Write PW\n");
+  scanf("%s", pw);
+
+  int pass = 0;
+  while (!pass) {
+    printf("Check PW\n");
+    scanf("%s", check);
+
+    int i = 0;
+    while (pw[i] != '\0') {
+      if (pw[i] == check[i])
+        i++;
+      else
+        break;
+    }
+    if (pw[i] == check[i])
+      pass = 1;
+    else
+      printf("PW check fail\n");
+  }
+  printf("PW check success\n");
+  printf("your email is cassandra@hanyang.ac.kr\n");
+}
---
+++ lab2-1/tile_flip.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void print_map(char arr[][5]) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (j < 4) {
+        printf("%c ", arr[i][j]);
+      } else {
+        printf("%c\n", arr[i][j]);
+      }
+    }
+  }
+}
+
+void flip(char (*arr)[5], int y, int x) {
+  for (int i = 0; i < 3 && y + i <= 5; i++) {
+    for (int j = 0; j < 3 && x + j <= 5; j++) {
+      if (arr[y + i - 1][x + j - 1] == 'O') {
+        arr[y + i - 1][x + j - 1] = 'X';
+      } else {
+        arr[y + i - 1][x + j - 1] = 'O';
+      }
+    }
+  }
+}
+
+int main() {
+  char arr[5][5];
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      arr[i][j] = 'X';
+    }
+  }
+
+  print_map(arr);
+  int x, y;
+  while (1) {
+    printf("\n");
+    printf("x : ");
+    scanf("%d", &x);
+    printf("y : ");
+    scanf("%d", &y);
+    flip(arr, y, x);
+    print_map(arr);
+
+    int cnt = 0;
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+        if (arr[i][j] == 'O')
+          cnt++;
+      }
+    }
+    if (cnt == 25) {
+      printf("complete!!\n");
+      return 0;
+    }
+  }
+}
